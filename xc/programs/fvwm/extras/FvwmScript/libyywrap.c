3c62334d27a6eb113fe01532d50973d92c3b1ca7
todd <todd@openbsd.org>
1998-03-25 08:07:34 +0000

    Import fvwm 2.0.46.  This will be default window manager for X for 2.3.

---
+++ xc/programs/fvwm/extras/FvwmScript/libyywrap.c
@@ -0,0 +1,8 @@
+/* libyywrap - flex run-time support library "yywrap" function */
+
+/* $Header: /tmp/OpenBSD-X11-repo/xc/programs/fvwm/extras/FvwmScript/libyywrap.c,v 1.1.1.1 1998/03/25 08:08:35 todd Exp $ */
+
+int yywrap()
+	{
+	return 1;
+	}
