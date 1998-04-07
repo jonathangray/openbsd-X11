9e48bb7dda662eaeedcb0d00c442fc069475bfc2
deraadt <deraadt@openbsd.org>
1998-04-07 10:13:21 +0000

    OpenBSD MANPATH=/usr/share/man:/usr/local/man:/usr/X11R6/man

--- contrib/programs/xman/vendor.h
+++ contrib/programs/xman/vendor.h
@@ -89,6 +89,9 @@ from the X Consortium.
 #ifdef __bsdi__
 #  define SYSMANPATH "/usr/share/man:/usr/contrib/man:/usr/contrib/isode/man:/usr/local/man"
 #endif /* __bsdi__ */
+#ifdef __OpenBSD__
+#  define SYSMANPATH "/usr/share/man:/usr/local/man:/usr/X11R6/man"
+#endif /* __OpenBSD__ */
 
 #ifndef SYSMANPATH
 #  define SYSMANPATH "/usr/man"
