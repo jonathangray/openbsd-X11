27a972e0ea54979f141c31a3242d8d0572e7498b
todd <todd@openbsd.org>
1998-06-27 14:41:03 +0000

    fix the headers right for the alpha where it's supposed to be fixed

--- xc/programs/fvwm/fvwm/misc.h
+++ xc/programs/fvwm/fvwm/misc.h
@@ -562,8 +562,7 @@ void fvwm_msg(int type,char *id,char *msg,...);
 #ifdef BROKEN_SUN_HEADERS
 #include "sun_headers.h"
 #endif
-#if defined(NEEDS_ALPHA_HEADER) && !defined(__OpenBSD__)
-#warning what
+#ifdef NEEDS_ALPHA_HEADER
 #include "alpha_header.h"
 #endif /* NEEDS_ALPHA_HEADER */
 #endif /* _MISC_ */
