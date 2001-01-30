3f195b49ab94f227e9ea8006746f6abc4c7830b8
todd <todd@openbsd.org>
2001-01-30 20:32:28 +0000

    use ssh-agent/ssh-add if ~/.ssh/identity or ~/.ssh/id_dsa exist

--- xc/programs/xinit/xinitrc.cpp
+++ xc/programs/xinit/xinitrc.cpp
@@ -1,5 +1,6 @@
 XCOMM!/bin/sh
 XCOMM $XConsortium: xinitrc.cpp,v 1.4 91/08/22 11:41:34 rws Exp $
+XCOMM $OpenBSD: xinitrc.cpp,v 1.3 2001/01/30 20:32:28 todd Exp $
 
 userresources=$HOME/.Xresources
 usermodmap=$HOME/.Xmodmap
@@ -24,9 +25,23 @@ if [ -f $usermodmap ]; then
     xmodmap $usermodmap
 fi
 
+XCOMM if we have private ssh key(s), start ssh-agent and add the key(s)
+id1=$HOME/.ssh/identity
+id2=$HOME/.ssh/id_dsa
+if [ -e /usr/bin/ssh-agent ] && [ -f $id1 -o -f $id2 ]; then
+	eval `ssh-agent -s`
+	[ -f $id1 ] && ssh-add $id1 < /dev/null
+	[ -f $id2 ] && ssh-add $id2 < /dev/null
+fi
+
 XCOMM start some nice programs
 
 xclock -geometry 50x50-1+1 &
 xconsole &
 xterm -geometry 80x24 &
-exec fvwm || exec xterm
+fvwm || xterm
+
+if [ "$SSH_AGENT_PID" ]; then
+	ssh-add -D < /dev/null
+	eval `ssh-agent -s -k`
+fi
